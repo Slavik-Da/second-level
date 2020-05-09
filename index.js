3499f53bfec94da4cb20ffb23f8bcfa3ebb8f349
Slavik-Da <61164494+Slavik-Da@users.noreply.github.com>
2020-05-09 14:18:37 +0300

    add then method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
 fetch(API_URL);
 console.log(responsePromise);
+responsePromise.then(response => { console.log(response) });
