caea58fc4b94b2c9de31e236398f0ccfd29e1eb0
Slavik-Da <61164494+Slavik-Da@users.noreply.github.com>
2020-05-09 16:31:01 +0300

    Update index.js
    Create fighter

--- index.js
+++ index.js
@@ -62,6 +62,17 @@ function createElement({ tagName, className = '', attributes = {} }) {
     return imgElement;
   }
 
+  function createFighter(fighter) {
+    const { name, source } = fighter;
+    const nameElement = createName(name);
+    const imageElement = createImage(source);
+    const element = createElement({ tagName: 'div', className: 'fighter' });
+  
+    element.append(imageElement, nameElement);
+  
+    return element;
+  }
+
 
 function getFightersNames(fighters) {
   const names = fighters.map(it => it.name).join('\n');
