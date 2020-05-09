a5a9c852f1af46e5b669096dc0605fb1dea61c01
Slavik-Da <61164494+Slavik-Da@users.noreply.github.com>
2020-05-09 14:19:36 +0300

    method then fix

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
 fetch(API_URL);
-console.log(responsePromise);
 responsePromise.then(response => { console.log(response) });
