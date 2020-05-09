3daa10456bc3ece10ba2fc1db99f71e3afc1dd2e
Slavik-Da <61164494+Slavik-Da@users.noreply.github.com>
2020-05-09 15:54:52 +0300

    add loading overlay

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://api.github.com/repos/sahanr/street--fighter/contents/fighters.json';
+const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
 const rootElement = document.getElementById('root');
+const loadingElement = document.getElementById('loading-overlay');
 
-rootElement.innerText = 'Loading...';
 fetch(API_URL)
 .then(response => {
     if (!response.ok) {
@@ -14,8 +14,12 @@ fetch(API_URL)
     const names = fighters.map(it => it.name).join('\n');
 
     rootElement.innerText = names;
+    loadingElement.remove();
+
 })
 .catch(error => {
     console.warn(error);
     root.innerText = 'Failed to load data';
+    loadingElement.remove();
+
   });
