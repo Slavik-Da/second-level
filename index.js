3a25ff15eb4b2ac38cbc986753c7ea3a59b1ffe6
Slavik-Da <61164494+Slavik-Da@users.noreply.github.com>
2020-05-09 15:43:31 +0300

    Update index.js
    catch method

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
+const API_URL = 'https://api.github.com/repos/sahanr/street--fighter/contents/fighters.json';
 const rootElement = document.getElementById('root');
 
 rootElement.innerText = 'Loading...';
 fetch(API_URL)
-.then(
-    response => { return response.json(); }, 
-    error => { console.warn(error); }
-  )
 .then(response => response.json())
 .then(file => {
     const fighters = JSON.parse(atob(file.content));
     const names = fighters.map(it => it.name).join('\n');
     rootElement.innerText = names;
-});
+})
+.catch(error => {
+    console.warn(error);
+    root.innerText = 'Failed to load data';
+  });
