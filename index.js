30920a0012960506495179ea58e161b21bbe4f3c
Slavik-Da <61164494+Slavik-Da@users.noreply.github.com>
2020-05-10 15:30:34 +0300

    Update index.js
    create view, fighter & fighters classes

--- index.js
+++ index.js
@@ -3,25 +3,8 @@ const rootElement = document.getElementById('root');
 const loadingElement = document.getElementById('loading-overlay');
 const fightersDetailsMap = new Map();
 
-
-async function startApp() {
-    try {
-      loadingElement.style.visibility = 'visible';
-      
-      const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
-      const fighters = await callApi(endpoint, 'GET');
-      const fightersElement = createFighters(fighters);
-      
-      rootElement.appendChild(fightersElement);
-    } catch (error) {
-      console.warn(error);
-      rootElement.innerText = 'Failed to load data';
-    } finally {
-      loadingElement.style.visibility = 'hidden';
-    }
-  }
   
-  function callApi(endpoind, method) {
+function callApi(endpoind, method) {
     const url = API_URL + endpoind
     const options = {
       method
@@ -51,3 +34,81 @@ async function startApp() {
   }
   
   const fighterService = new FighterService();
+
+  class View {
+    element;
+  
+    createElement({ tagName, className = '', attributes = {} }) {
+      const element = document.createElement(tagName);
+      element.classList.add(className);
+      
+      Object.keys(attributes).forEach(key => element.setAttribute(key, attributes[key]));
+  
+      return element;
+    }
+  }
+
+
+  class FighterView extends View {
+    constructor(fighter, handleClick) {
+      super();
+  
+      this.createFighter(fighter, handleClick);
+    }
+  
+    createFighter(fighter, handleClick) {
+      const { name, source } = fighter;
+      const nameElement = this.createName(name);
+      const imageElement = this.createImage(source);
+  
+      this.element = this.createElement({ tagName: 'div', className: 'fighter' });
+      this.element.append(imageElement, nameElement);
+      this.element.addEventListener('click', event => handleClick(event, fighter), false);
+    }
+  
+    createName(name) {
+      const nameElement = this.createElement({ tagName: 'span', className: 'name' });
+      nameElement.innerText = name;
+  
+      return nameElement;
+    }
+  
+    createImage(source) {
+      const attributes = { src: source };
+      const imgElement = this.createElement({
+        tagName: 'img',
+        className: 'fighter-image',
+        attributes
+      });
+  
+      return imgElement;
+    }
+  }
+  
+  class FightersView extends View {
+    constructor(fighters) {
+      super();
+      
+      this.createFighters(fighters);
+    }
+  
+    fightersDetailsMap = new Map();
+  
+    createFighters(fighters) {
+      const fighterElements = fighters.map(fighter => {
+        const fighterView = new FighterView(fighter, this.handleFighterClick);
+        return fighterView.element;
+      });
+  
+      this.element = this.createElement({ tagName: 'div', className: 'fighters' });
+      this.element.append(...fighterElements);
+    }
+  
+    handleFighterClick(event, fighter) {
+      this.fightersDetailsMap.set(fighter._id, fighter);
+      console.log('clicked')
+      // get from map or load info and add to fightersMap
+      // show modal with fighter info
+      // allow to edit health and power in this modal
+    }
+  }
